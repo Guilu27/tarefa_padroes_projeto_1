343e615f2c1afa80cfeedcab39d2faeda3a3cf8e
Guilherme <turtwig102@gmail.com>
2024-01-05 14:37:48 -0300

    feat(MercedesEngine): adiciona produto concreto MercedesEngine.java
    Implementa um produto concreto `MercedesEngine.java` que estende `Engine.java`. Sobrescreve os métodos `horsepower` e `cylinderConfiguration` para personalização específica de motores Mercedes.

---
+++ src/Engines/MercedesEngine.java
@@ -0,0 +1,13 @@
+package Engines;
+
+public class MercedesEngine extends Engine {
+    @Override
+    public void horsepower() {
+        System.out.println("450 cavalos");
+    }
+
+    @Override
+    public void cylinderConfiguration() {
+        System.out.println("V6");
+    }
+}
