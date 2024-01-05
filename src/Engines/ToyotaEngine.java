959edfcdc67cc362ccd9932d21a89cb6ff73e262
Guilherme <turtwig102@gmail.com>
2024-01-05 14:45:34 -0300

    feat(ToyotaEngine): adiciona produto concreto ToyotaEngine.java
    Implementa um produto concreto `ToyotaEngine.java` que estende `Engine.java`. Sobrescreve os métodos `horsepower` e `cylinderConfiguration` para personalização específica dos motores Toyota.

---
+++ src/Engines/ToyotaEngine.java
@@ -0,0 +1,13 @@
+package Engines;
+
+public class ToyotaEngine extends Engine {
+    @Override
+    public void horsepower() {
+        System.out.println("140 cavalos");
+    }
+
+    @Override
+    public void cylinderConfiguration() {
+        System.out.println("L4");
+    }
+}
