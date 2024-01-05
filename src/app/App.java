74ae778fd897930e0ed926b76c68ded4dbf22e53
Guilherme <turtwig102@gmail.com>
2024-01-05 15:13:20 -0300

    chore(app): atualiza classe App.java para utilizar Factory
    Refatora a classe `App.java` para incorporar o padrão de fábrica. Adiciona um construtor que recebe uma instância da `CarFactory` para criar os produtos necessários (carro, motor e pneu). Atualiza o método `createFullCar` para utilizar os produtos criados pela fábrica.

--- src/app/App.java
+++ src/app/App.java
@@ -3,11 +3,18 @@ package app;
 import Cars.Car;
 import Engines.Engine;
 import Tires.Tire;
+import factories.CarFactory;
 
 public class App {
     private Car car;
     private Engine engine;
     private Tire tire;
+
+    public App(CarFactory factory) {
+        this.car = factory.createCar();
+        this.engine = factory.createEngine();
+        this.tire = factory.createTire();
+    }
 
     public void createFullCar() {
         car.carModel();
